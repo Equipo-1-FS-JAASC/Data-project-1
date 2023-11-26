5498b042957beacdce02fb0802df36d12f0a2f96
cmarty02 <cristianvmarty@gmail.com>
2023-11-27 00:42:36 +0100

    Montaje_BBDD_docker

---
+++ bbdd/docker_bbdd/fill_data_script.py
@@ -0,0 +1,74 @@
+import pandas as pd
+from faker import Faker
+import random
+from datetime import datetime, timedelta
+import psycopg2
+from psycopg2 import sql  
+import time
+
+time.sleep(10)
+
+# Configuración de Faker
+fake = Faker('es_ES')
+
+# Configuración del generador de números aleatorios
+random.seed(42)
+
+# Crear una lista para almacenar los datos
+data = []
+
+# Generar 1000 registros
+for _ in range(1000):
+    dni = random.randint(10000000, 99999999)  # Generar DNI de 8 dígitos
+    nombre = fake.first_name()
+    apellido = fake.last_name()
+    edad = random.randint(70, 90)
+    fecha_nacimiento = fake.date_of_birth(minimum_age=edad)
+    id_solicitud = random.randint(1, 1000)
+    usuario_solicitante = fake.boolean()
+    oficio_especial = fake.boolean()
+
+    data.append([dni, nombre, apellido, edad, fecha_nacimiento, id_solicitud, usuario_solicitante, oficio_especial])
+
+# Crear un DataFrame con los datos
+df = pd.DataFrame(data, columns=['dni', 'nombre', 'apellido', 'edad', 'fecha_de_nacimiento', 'id_solicitud', 'usuario_solicitante', 'oficio_especial'])
+
+
+#Coonexion a BBDD............................................................................
+
+dbname = "DBInmerso"
+user = "postgres"
+password = "postgres"
+host = "postgres"
+port = "5432"
+
+# Crear una conexión
+conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host, port=port)
+
+try:
+
+    # Crear una consulta de inserción
+    insert_query = sql.SQL("INSERT INTO usuarios ({}) VALUES ({})").format(
+        sql.SQL(', ').join(map(sql.Identifier, df.columns)),
+        sql.SQL(', ').join(sql.Placeholder() * len(df.columns))
+    )
+
+    # Obtener el cursor
+    cursor = conn.cursor()
+
+    # Insertar filas del DataFrame en la tabla de la base de datos
+    for _, row in df.iterrows():
+        cursor.execute(insert_query, tuple(row))
+
+    # Confirmar la transacción
+    conn.commit()
+
+    print("Datos insertados correctamente en la tabla.")
+
+except Exception as e:
+    print(f"Error: {e}")
+
+finally:
+    # Cerrar la conexión
+    if conn is not None:
+        conn.close()
---
+++ bbdd/docker_bbdd/init.sql
@@ -0,0 +1,125 @@
+-- Eliminar tablas si existen
+DROP TABLE IF EXISTS usuarios;
+DROP TABLE IF EXISTS historial_usuarios;
+DROP TABLE IF EXISTS renta;
+DROP TABLE IF EXISTS scoring;
+DROP TABLE IF EXISTS tipo_resolucion;
+DROP TABLE IF EXISTS disponibilidad;
+DROP TABLE IF EXISTS discapacidad;
+DROP TABLE IF EXISTS familia_numerosa;
+DROP TABLE IF EXISTS patrimonio;
+DROP TABLE IF EXISTS hoteles;
+DROP TABLE IF EXISTS destinos;
+DROP TABLE IF EXISTS resolucion_solicitudes;
+DROP TABLE IF EXISTS solicitudes;
+
+
+
+-- Crear tabla usuarios
+CREATE TABLE usuarios (
+    dni VARCHAR(9) PRIMARY KEY,
+    nombre VARCHAR(255),
+    apellido VARCHAR(255),
+    edad INT,
+    fecha_de_nacimiento DATE,
+    id_solicitud INT,
+    usuario_solicitante BOOLEAN,
+    oficio_especial BOOLEAN
+);
+
+-- Crear tabla historial_usuarios
+CREATE TABLE historial_usuarios (
+    dni VARCHAR(9) PRIMARY KEY,
+    cross_selling BOOLEAN,
+    resultado_solicitud_t_1 INT,
+    viajes_t_1 INT,
+    resultado_solicitud_t_2 INT,
+    viajes_t_2 INT,
+    suma_viajes_t_1_y_t_2 INT,
+    scoring_ind_participacion_previa FLOAT
+);
+
+-- Crear tabla renta
+CREATE TABLE renta (
+    dni VARCHAR(9) PRIMARY KEY,
+    ingresos FLOAT
+);
+
+
+-- Crear tabla scoring
+CREATE TABLE scoring (
+    id_solicitud VARCHAR(9) PRIMARY KEY,
+    total FLOAT,
+    edad FLOAT,
+    discapacidad FLOAT,
+    ingresos FLOAT,
+    agregado_part_previa FLOAT,
+    fam_num FLOAT
+);
+
+-- Crear tabla tipo_resolucion
+CREATE TABLE tipo_resolucion (
+    id_resolucion INT PRIMARY KEY,
+    tipo_resolucion VARCHAR(255)
+);
+
+-- Crear tabla disponibilidad
+CREATE TABLE disponibilidad (
+    id_hotel INT PRIMARY KEY,
+    fecha_disponibilidad_hab DATE,
+    num_hab_disp INT
+);
+
+-- Crear tabla discapacidad
+CREATE TABLE discapacidad (
+    dni VARCHAR(9) PRIMARY KEY,
+    grado_dis INT
+);
+
+-- Crear tabla familia_numerosa
+CREATE TABLE familia_numerosa (
+    dni VARCHAR(9) PRIMARY KEY,
+    tipo_fam_num INT
+);
+
+-- Crear tabla patrimonio
+CREATE TABLE patrimonio (
+    dni VARCHAR(9) PRIMARY KEY,
+    valoracion_patrimonio FLOAT,
+    coche BOOLEAN,
+    arrendador BOOLEAN
+);
+
+-- Crear tabla hoteles
+CREATE TABLE hoteles (
+    id_hotel INT PRIMARY KEY,
+    nombre VARCHAR(255),
+    ciudad VARCHAR(255)
+);
+
+CREATE TABLE destinos (
+    id_destinos INT PRIMARY KEY,
+    tipo_destino VARCHAR(255),
+    plazas_destino INT
+    );
+
+CREATE TABLE resolucion_solicitudes (
+    id_solicitud INT PRIMARY KEY,
+    anyo_solicitud INT,
+    usuarios_sol VARCHAR(255),
+    renta_sol INT,
+    primera_opcion VARCHAR(255),
+    segunda_opcion VARCHAR(255),
+    tercera_opcion VARCHAR(255)
+);
+
+-- Crear tabla solicitudes
+CREATE TABLE solicitudes (
+    id_solicitud INT PRIMARY KEY,
+    anyo_solicitud INT,
+    usuarios_sol VARCHAR(255),
+    renta_sol INT,
+    primera_opcion VARCHAR(255),
+    segunda_opcion VARCHAR(255),
+    tercera_opcion VARCHAR(255)
+);
